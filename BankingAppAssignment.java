6643063f6a62bd7abb0f4c289ac6f6d5d35042ea
Shohan9930 <sshohan9930@gmail.com>
2023-08-15 23:44:24 +0530

    last commit

--- BankingAppAssignment.java
+++ BankingAppAssignment.java
@@ -18,7 +18,8 @@ public class BankingAppAssignment {
 
         String[] acholnames = new String[0];
         String screen = DASHBOARD;
-        String[] accid = new String[0];
+        int[] initialdepos=new int[0];
+        String[] accids = new String[0];
 
         do {
             final String APP_TITLE = String.format("%s%s%s",
@@ -49,17 +50,27 @@ public class BankingAppAssignment {
                         case 4: screen = TRANSFER; break;
                         case 5: screen = CHECK_AC_BALANCE; break;
                         case 6: screen = DELETE_ACCOUNT; break;
-                        case 7: System.exit(0); break;
-                        default: continue;
+                        case 7: System.out.println(CLEAR);System.exit(0);break;
+                        default:continue;
                     }
                     break;
 
                     case CREATE_ACCOUNT:
-                    System.out.printf("New Account ID: SDB-%06d \n", (accid.length + 1));
+                    System.out.println(CLEAR);
+                    String accid = String.format("SDB-%06d", (accids.length+1));
+
+                    System.out.println("New Account ID: "+accid);
 
                     boolean valid;
                     String acholname;
                     int initialdepo;
+                    String [] newaccids = new String [accids.length + 1];
+                    for (int i = 0; i < accids.length; i++) {
+                        newaccids[i] = accids[i];
+                    }
+                    newaccids[newaccids.length - 1] = accid;
+                    accids = newaccids;
+                    
                     do{
                         valid = true;
                         System.out.print("Enter Account Holder's Name: ");
@@ -77,12 +88,21 @@ public class BankingAppAssignment {
                                 break;
                             }
                         }
+
+                        
                     }while(!valid);
+
+                    String[] newacholnames = new String[acholnames.length + 1];
+                    for (int i = 0; i < acholnames.length; i++) {
+                        newacholnames[i] = acholnames[i];
+                    }
+                    newacholnames[newacholnames.length - 1] = acholname;
+                    acholnames = newacholnames;
 
                     do{
                         
                         valid = true;
-                        System.out.print("Enter Initial DEposit Amount: ");
+                        System.out.print("Enter Initial Deposit Amount: ");
                         initialdepo = SCANNER.nextInt();
                         if (initialdepo<5000){
                             System.out.printf("%sInsufficient Initial Deposit Amount:You should deposit at least RS.5000 as initial amount%s\n", COLOR_RED_BOLD, RESET);
@@ -91,7 +111,24 @@ public class BankingAppAssignment {
                         }
                         
                     }while(!valid);
-                            
+                    int [] newinitialdepos = new int [initialdepos.length + 1];
+                    for (int i = 0; i < initialdepos.length; i++) {
+                        newinitialdepos[i] = initialdepos[i];
+                    }
+                    newinitialdepos[newinitialdepos.length - 1] = initialdepo;
+                    initialdepos = newinitialdepos;
+                    
+
+                    System.out.println();
+                    System.out.print(accid+":"+acholname + " created an account sucessfully. Do you want to add new student (Y/n)? ");
+                    String response=SCANNER.nextLine().strip().toUpperCase();
+
+                    if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) continue;
+                    screen = DASHBOARD;
+                    break;
+                    
+                default:
+                    System.exit(0);
 
 
         }
