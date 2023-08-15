19a1619c896baed2368f5574ad99ae65c04d5d6e
Shohan9930 <sshohan9930@gmail.com>
2023-08-15 21:27:34 +0530

    First commit

---
+++ BankingAppAssignment.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BankingAppAssignment {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    public static void main(String[] args) {
+        final String CLEAR = "\033[H\033[2J";
+        final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
+        final String RESET = "\033[0m";
+
+        final String DASHBOARD = "Welcome to SMART  BANKING";
+        final String CREATE_ACCOUNT = "Create New Account";
+
+        String[] name = new String[0];
+        String screen = DASHBOARD;
+
+        
+
+
+
+
+
+        
+
+
+    }
+    
+}
